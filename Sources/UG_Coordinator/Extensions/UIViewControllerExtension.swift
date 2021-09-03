8d75db83b03898f904dc80d00f3051d9cf1b4853
andrey.goryunov <andrey.goryunov@simbirsoft.com>
2021-09-03 11:46:10 +0400

    Add extension

---
+++ Sources/UG_Coordinator/Extensions/UIViewControllerExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewControllerExtension.swift
+//  
+//
+//  Created by Andrey Goryunov on 03.09.2021.
+//
+
+import UIKit
+
+extension UIViewController: Presentable {
+    
+    public func toPresent() -> UIViewController? {
+        self
+    }
+    
+}
